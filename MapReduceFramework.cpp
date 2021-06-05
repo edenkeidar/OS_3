7432fb5c9145303f5569e55ab8e6b85aa0d789d6
mayam <mayamoriya@gmail.com>
2021-06-05 15:29:02 +0300

    5 threads prints

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -3,6 +3,7 @@
 //
 #include "MapReduceClient.h"
 #include "MapReduceFramework.h"
+#include "Sample_Client\SampleClient.cpp"
 #include <pthread.h>
 #include <vector>
 #include <string>
@@ -14,6 +15,7 @@ using namespace std;
 #define THREAD_CREATE_ERROR "system couldn't create thread: "
 #define FAIL 1
 #define SUCCESS 0
+
 struct Context{
     IntermediateVec* intermediary_elements;
     OutputVec* output_elements;
@@ -45,16 +47,23 @@ void emit3 (K3* key, V3* value, void* context){
     return;
 }
 
-
-
 JobHandle startMapReduceJob(const MapReduceClient& client,
                             const InputVec& inputVec, OutputVec& outputVec,
                             int multiThreadLevel){
+
     Job* new_job;
     new_job->threads = new pthread_t[multiThreadLevel];
     new_job->contexts = new Context[multiThreadLevel];
-    for (int i = 0; i < multiThreadLevel; ++i) {
-        int err = pthread_create(&new_job->threads[i], NULL, &f, NULL);
+
+    //thread 0
+    int err = pthread_create(&new_job->threads[0], NULL, &main_thread_entry, NULL);
+    handle_error(err, THREAD_CREATE_ERROR);
+    new_job->contexts[0].intermediary_elements =  new IntermediateVec;
+    new_job->contexts[0].output_elements = new OutputVec;
+
+    // rest of threads
+    for (int i = 1; i < multiThreadLevel; ++i) {
+        int err = pthread_create(&new_job->threads[i], NULL, &basic_thread_entry, NULL);
         handle_error(err, THREAD_CREATE_ERROR);
         new_job->contexts[i].intermediary_elements =  new IntermediateVec;
         new_job->contexts[i].output_elements = new OutputVec;
@@ -91,15 +100,54 @@ void* basic_thread_entry(void *){
     sort();
     wait_for_shuffle();
     reduce();
-}
-
-void* f(void *){
-    printf("!\n");
     return nullptr;
 }
+
 void* main_thread_entry(void *){
     map();
     sort();
     shuffle();
     reduce();
+    return nullptr;
+}
+
+
+void* f(void *){
+    printf("!\n");
+    return nullptr;
+}
+
+void map(){
+    printf("1\n");
+    return;
+}
+
+void sort(){
+    printf("2\n");
+    return;
+}
+
+void shuffle() {
+    printf("3\n");
+    return;
+}
+
+void reduce(){
+    printf("5\n");
+    return;
+}
+
+void wait_for_shuffle(){
+    printf("4\n");
+    return;
+}
+
+
+int main(int argc, char** argv){
+    //CounterClient client = new CounterClient()
+    CounterClient client;
+    InputVec inputVec;
+    OutputVec outputVec;
+    JobHandle out = startMapReduceJob(client, inputVec, outputVec, 5);
+    return 0;
 }
